bc8dca44b9373551a88b80e12d29fde5b85916fb
Miroslav Dimitrov <mdimitrov963@gmail.com>
2025-11-16 17:53:07 +0200

    new logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "./global.css";
+import BulgariaLogo from "../public/Bulgaria-Taste-Logo.png";
 function App() {
   return (
     <>
       <div className="text-2xl text-red-600">Bulgaria Taste</div>
+      <img src={BulgariaLogo} alt="Bulgaria Taste Logo" />
     </>
   );
 }
