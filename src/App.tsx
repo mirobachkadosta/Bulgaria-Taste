4d9f70c512fc6dae6aec662b96639ef9337d1b45
Miroslav Dimitrov <mdimitrov963@gmail.com>
2025-11-18 19:38:24 +0200

    tailwind fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import "./global.css";
 import Header from "./components/Header/header";
+import { Route, Routes } from "react-router";
+import Home from "./components/Home/home";
+import "./global.css";
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<div>About Page</div>} />
+        <Route path="/contact" element={<div>Contact Page</div>} />
+        <Route path="/restaurants" element={<div>Restaurants Page</div>} />
+      </Routes>
     </>
   );
 }
---
+++ src/components/Home/home.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className="content-container bg-primary">
+      <div className="bg-primary">
+        <h1 className="">Открий истинския вкус на България</h1>
+        <h2>
+          Пътувай, събирай километри, отключвай отстъпки и ваучери в реални
+          заведения.
+        </h2>
+      </div>
+    </div>
+  );
+}
