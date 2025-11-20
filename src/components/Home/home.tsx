4a87e8798bc5d628b3c1cc876f1508d8fa5e2b63
Miroslav Dimitrov <mdimitrov963@gmail.com>
2025-11-20 19:34:24 +0200

    add why us and how exactly works section

--- src/components/Home/home.tsx
+++ src/components/Home/home.tsx
@@ -1,8 +1,10 @@
-import HowItWorks from "../HowItWorks/HowItWorks";
+import HowItWorks from "../HomePage/HowItWorks/HowItWorks";
+import WhyUsSection from "../HomePage/WhyUs/WhyUs";
 
 export default function Home() {
   return (
     <div className="content-container">
+      <WhyUsSection />
       <HowItWorks />
       {/* <div className="text-primary-content">
         <h1>Открий истинския вкус на България</h1>
---
+++ src/components/HomePage/HowItWorks/HowItWorks.tsx
@@ -0,0 +1,30 @@
+import { HOW_IT_WORKS_VALUES } from "@/utility/staticValues";
+import { HeaderSection } from "../../HeaderSection/HeaderSepartor";
+
+export default function HowItWorks() {
+  return (
+    <section className="bg-base-100 full-width-section lg:pb-12 pb-6">
+      <div className="content-container flex flex-col justify-center items-center text-center gap-6">
+        <HeaderSection
+          className="lg:pt-12! pt-6!"
+          header="Как точно работи?"
+          typeOfHeader="h2"
+        />
+        <div className="grid lg:grid-cols-4 gap-12 sm:grid-cols-2 grid-cols-1 items-start">
+          {HOW_IT_WORKS_VALUES.map((item, index) => (
+            <div
+              key={index}
+              className="flex flex-col justify-center items-center gap-3"
+            >
+              <div className="bg-primary size-20 rounded-full flex items-center justify-center">
+                {item.icon}
+              </div>
+              <h3 className="font-bold">{item.header}</h3>
+              <p>{item.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
