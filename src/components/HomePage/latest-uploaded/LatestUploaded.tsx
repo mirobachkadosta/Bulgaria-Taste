b17525525d8edee3360ae8fca5e1e0696b7047c5
Miroslav Dimitrov <mdimitov963@gmail.com>
2025-11-21 21:40:46 +0200

    issues with supabase fix them later

--- src/components/HomePage/latest-uploaded/LatestUploaded.tsx
+++ src/components/HomePage/latest-uploaded/LatestUploaded.tsx
@@ -1,7 +1,33 @@
+import { Button } from "@/components/ui/button";
+import { supabase } from "@/supabase";
+
 export default function LatestUploaded() {
+  const addRestaurant = async () => {
+    const newRestaurant = {
+      id: 1,
+      created_at: new Date(),
+      name: "Test Restaurant",
+      location_id: 1,
+      type_of_food_id: 1,
+      logo: "",
+      images: "",
+    };
+
+    const { data, error } = await supabase
+      .from("restaurants")
+      .insert([newRestaurant])
+      .single();
+
+    if (error) {
+      console.error("Error adding restaurant:", error);
+    } else {
+      console.log("Restaurant added:", data);
+    }
+  };
   return (
     <section className="flex justify-center items-center bg-base-100">
       <h2 className="font-bold">Последно добавени ресторанти</h2>
+      <Button onClick={addRestaurant}>Add Test Restaurant</Button>
     </section>
   );
 }
