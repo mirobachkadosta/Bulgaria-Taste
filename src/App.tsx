b54410083a404d85b2b1422901f38ef88bd1de58
Miroslav Dimitrov <mdimitov963@gmail.com>
2025-11-25 20:46:24 +0200

    create about us page

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,33 @@ import Header from "./components/header/Header";
 import { Route, Routes } from "react-router";
 import Home from "./components/Home/home";
 import "./global.css";
+import Footer from "./components/footer/Footer";
+import AboutUs from "./components/about-us/AboutUs";
+import {
+  restaurantsLink,
+  aboutUsLink,
+  registerLink,
+  contactUsLink,
+} from "./utility/links";
 function App() {
   return (
     <>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<div>About Page</div>} />
-        <Route path="/contact" element={<div>Contact Page</div>} />
-        <Route path="/restaurants" element={<div>Restaurants Page</div>} />
+        <Route
+          path={aboutUsLink}
+          element={
+            <div>
+              <AboutUs />
+            </div>
+          }
+        />
+        <Route path={contactUsLink} element={<div>Contact Page</div>} />
+        <Route path={restaurantsLink} element={<div>Restaurants Page</div>} />
+        <Route path={registerLink} element={<div>Register Page</div>} />
       </Routes>
+      <Footer />
     </>
   );
 }
---
+++ src/utility/links.tsx
@@ -0,0 +1,4 @@
+export const restaurantsLink = "/restaurants";
+export const aboutUsLink = "/about";
+export const registerLink = "/register";
+export const contactUsLink = "/contact";
