da14baf249c273c517e66bf91b9d6fd1cd8f13f6
Miroslav Dimitrov <mdimitov963@gmail.com>
2025-12-05 09:09:01 +0200

    test

---
+++ src/components/restaurants/restaurant-details/RestaurantDetails.tsx
@@ -0,0 +1,129 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import type { RestaurantCardType, User } from "../../../utility/types";
+import { supabase } from "../../../supabase/supabase";
+import { globalStore } from "@/store/globalStore";
+import { Button } from "@/components/ui/button";
+
+export default function RestaurantDetails() {
+  const { id } = useParams();
+  const [restaurant, setRestaurant] = useState<RestaurantCardType | null>(null);
+  const [foodNames, setFoodNames] = useState<string[]>([]);
+  const { setIsLoading, user } = globalStore();
+  const currentUser = user as User;
+  const [actionMade, setActionMade] = useState(false);
+  useEffect(() => {
+    const fetchRestaurant = async () => {
+      setIsLoading(true);
+      const { data, error } = await supabase
+        .from("restaurants")
+        .select("*")
+        .eq("id", id)
+        .single();
+      if (!error && data) {
+        setRestaurant(data);
+        if (
+          data.types_of_food &&
+          Array.isArray(data.types_of_food) &&
+          data.types_of_food.length > 0
+        ) {
+          const { data: foodData, error: foodError } = await supabase
+            .from("type_food")
+            .select();
+          if (!foodError && foodData) {
+            const names = data.types_of_food
+              .map(
+                (id: string) => foodData.find((ft: any) => ft.id == id)?.name
+              )
+              .filter(Boolean);
+            setFoodNames(names);
+          }
+        }
+      }
+      setIsLoading(false);
+      setIsLoading(false);
+    };
+    fetchRestaurant();
+  }, [id, setIsLoading]);
+
+  const handleLike = async () => {
+    if (!restaurant) return;
+
+    const { data, error } = await supabase
+      .from("restaurants")
+      .upsert({ id: restaurant.id, like: restaurant.like + 1 })
+      .select();
+
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    setActionMade(true);
+    setRestaurant({
+      ...restaurant,
+      like: restaurant.like + 1,
+    });
+  };
+
+  const handleDislike = async () => {
+    if (!restaurant) return;
+
+    const { data, error } = await supabase
+      .from("restaurants")
+      .upsert({ id: restaurant.id, dislike: restaurant.dislike + 1 })
+      .select();
+
+    if (error) {
+      console.error(error);
+      return;
+    }
+    setActionMade(true);
+    setRestaurant({
+      ...restaurant,
+      dislike: restaurant.dislike + 1,
+    });
+  };
+  console.log(restaurant?.logo);
+
+  return (
+    <main className="full-width-section bg-base-200">
+      <div className="p-6 max-w-2xl mx-auto rounded-xl bg-base-100 shadow-md flex flex-col items-center">
+        {restaurant?.logo && (
+          <img
+            src={restaurant.logo}
+            alt={restaurant.name || "Logo"}
+            className="w-32 h-32 object-cover mb-4 rounded-full"
+          />
+        )}
+        <h2 className="text-2xl font-bold mb-2">{restaurant?.name}</h2>
+        <p className="mb-2">Location: {restaurant?.location?.name}</p>
+        <p className="mb-2">
+          Types of food: {foodNames.length ? foodNames.join(", ") : "-"}
+        </p>
+        <div className="flex gap-4 mb-4">
+          <span>Likes: {restaurant?.like}</span>
+          <span>Dislikes: {restaurant?.dislike}</span>
+        </div>
+        {currentUser.email !== restaurant?.creator_email ? (
+          <div className="flex gap-4 mt-4">
+            <Button
+              disabled={actionMade}
+              onClick={() => handleLike()}
+              className="px-4 py-2 bg-green-500 text-white rounded flex items-center gap-2"
+            >
+              👍 <span>Харесвам</span>
+            </Button>
+            <Button
+              disabled={actionMade}
+              onClick={() => handleDislike()}
+              className="px-4 py-2 bg-red-500 text-white rounded flex items-center gap-2"
+            >
+              👎 <span>Не харесвам</span>
+            </Button>
+          </div>
+        ) : null}
+      </div>
+    </main>
+  );
+}
